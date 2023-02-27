b2af24599c26447a9be2eadc42f285c80f6b49c0
vent-dev <cfpixel11@gmail.com>
2023-02-27 16:02:09 +0000

    Added about-me section and multiple icons

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let slides = document.getElementsByClassName("slides");
 
+let slideIndex = 0;
+
+function showSlides(){
+
+    for (let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    if (slideIndex >= slides.length){ slideIndex = 0; }
+    slides[slideIndex].style.display = "flex";
+    slideIndex++;
+    setTimeout(showSlides, 2000)
+}
+
+showSlides();
